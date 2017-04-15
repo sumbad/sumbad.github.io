5893f1e7ba079d05406754d0a4ad59e46c0c13e7
sumbad <sumbad@ya.ru>
2017-04-15 18:00:51 +0800

    adder first post - vue-web-components

--- src/Gist.vue
+++ src/Gist.vue
@@ -1,7 +1,7 @@
 <template>
   <iframe :id="iframeId"
           scrolling="no"
-          style="width:100%">
+          style="width:100%; height:0">
   </iframe>
 </template>
 
@@ -35,11 +35,16 @@ export default class Gist extends Vue {
             font-size: 14px !important;
           }
         </style>
-        <body onload="parent.adjustIframeSize('${this.iframeId}',document.body.scrollHeight)">
+        <body>
+          <link rel="stylesheet" href="https://assets-cdn.github.com/assets/gist-embed-9f0a4ad9c85ca776e669003688baa9d55f9db315562ce4d231d37dab2714c70a.css">
           <scr`+`ipt type="text/javascript" src="https://gist.github.com/sumbad/${this.iframeId}.js"></sc`+`ript>
         </body>
       </html>`);
       doc.close();
+
+    this.$el['contentWindow'].onload = () => {
+      this.$el.style.height = doc.body.scrollHeight + "px";
+    }
   }
 }
 </script>
