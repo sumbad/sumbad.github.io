41c7445e3d52aa330a66e18eb62cb3c5d7e496f5
sumbad <sumbad@ya.ru>
2017-08-21 23:24:16 +0800

    added hyperhtml

--- src/main.tsx
+++ src/main.tsx
@@ -3,65 +3,81 @@ import './polyfills';
 import './styles/main.scss';
 import './public/font-awesome/scss/font-awesome.scss';
 
+
+import * as hyperHTML from  'hyperhtml';
+
 const { h } = require('dom-chef');
 
 
 export class AppComponent extends HTMLElement {
+  html: any;
+
+  constructor(...args) {
+    super(...args);
+    this.html = hyperHTML.bind(this);
+  }
+
+  render() {
+    return this.html`<h1>Hello, ${this.getAttribute('name')}</h1>`;
+  }
+  
   connectedCallback() {
 
-    const el = (
-      <div class="header">
-        <a href="#" class="link">Download</a>
-      </div>
-    );
+    this.render();
 
-    // const $template =
-    //   <template id="task">
-    //     <li>
-    //       <h1>Task name</h1>
-    //       <p>Task body</p>
-    //     </li>
-    //   </template>;
+  //   const el = (
+  //     <div class="header">
+  //       <a href="#" class="link">Download</a>
+  //     </div>
+  //   );
 
-    const tasks = [
-      {
-        name: "Сделать зарядку",
-        body: "Разминка, пробежка, подтягивание на перекладине"
-      },
-      {
-        name: "Купить продукты",
-        body: "Хлеб, овощи, чай"
-      },
-      {
-        name: "Подготовить доклад",
-        body: "Найти проблему, описать суть, найти решение"
-      }
-    ];
+  //   // const $template =
+  //   //   <template id="task">
+  //   //     <li>
+  //   //       <h1>Task name</h1>
+  //   //       <p>Task body</p>
+  //   //     </li>
+  //   //   </template>;
 
-    let $todoList = <ui id="todo-list"></ui>
+  //   const tasks = [
+  //     {
+  //       name: "Сделать зарядку",
+  //       body: "Разминка, пробежка, подтягивание на перекладине"
+  //     },
+  //     {
+  //       name: "Купить продукты",
+  //       body: "Хлеб, овощи, чай"
+  //     },
+  //     {
+  //       name: "Подготовить доклад",
+  //       body: "Найти проблему, описать суть, найти решение"
+  //     }
+  //   ];
 
-    // const $content = document.importNode($template, true);
+  //   let $todoList = <ui id="todo-list"></ui>
 
-    // $content.querySelector('h1').innerText = '1111111111111111';
+  //   // const $content = document.importNode($template, true);
 
-    const $template: any = document.getElementById('task');
+  //   // $content.querySelector('h1').innerText = '1111111111111111';
 
-    tasks.forEach(task => {
-      const $clone = document.importNode($template.content, true);
+  //   const $template: any = document.getElementById('task');
+
+  //   tasks.forEach(task => {
+  //     const $clone = document.importNode($template.content, true);
       
-      $clone.querySelector('h1').innerText = task.name;
-      $clone.querySelector('p').innerText = task.body;
-      $todoList.appendChild($clone);
-    });
+  //     $clone.querySelector('h1').innerText = task.name;
+  //     $clone.querySelector('p').innerText = task.body;
+  //     $todoList.appendChild($clone);
+  //   });
 
-    // let el1 = (
+  //   // let el1 = (
 
-    // );
+  //   // );
 
-    // this.innerHTML = el1;
-    this.appendChild($todoList);
+  //   // this.innerHTML = el1;
+  //   this.appendChild($todoList);
 
-    // this.appendChild(el);
+  //   // this.appendChild(el);
   }
 }
 
